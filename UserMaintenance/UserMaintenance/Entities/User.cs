59f3eb64f8deb607cdc5778c5788078f0b701429
mboti2000 <mboti20000@gmail.com>
2020-09-25 09:21:48 +0200

    users commit

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace UserMaintenance.Entities
 {
     public class User
     {
+        
         public Guid ID { get; set; } = Guid.NewGuid();
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,19 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
 
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add; 
             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User();
+         
+            u.LastName = textBox1.Text;
+            u.FirstName = textBox2.Text;
+            users.Add(u);
         }
     }
 }
