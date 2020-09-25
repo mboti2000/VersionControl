64783e2dbe8493cb682f7f0637d509d223e19538
mboti2000 <mboti20000@gmail.com>
2020-09-25 13:34:51 +0200

    file wrting 2 commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,8 @@ namespace UserMaintenance
             listBox1.DisplayMember = "FullName";
 
             label1.Text = Resource1.FullName;
-            button1.Text = Resource1.Add; 
+            button1.Text = Resource1.Add;
+            button2.Text = Resource1.WriteToFile;
             
         }
 
@@ -34,6 +36,27 @@ namespace UserMaintenance
             u.FullName = textBox1.Text;
             
             users.Add(u);
+            textBox1.Text = "";
+        }
+
+   
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(";");
+                    sw.Write(u.FullName);
+                    sw.Write(";");
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
