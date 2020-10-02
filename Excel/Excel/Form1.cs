7f528c5185d61abed2ff863441fd3357f068b708
mboti2000 <mboti20000@gmail.com>
2020-10-02 15:30:09 +0200

    basics commit

--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Excel
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData() {
+            Flats = context.Flats.ToList();
         }
     }
 }
