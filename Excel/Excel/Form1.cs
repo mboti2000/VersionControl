12061dfc48ad8eed83931c2c248d6d2648df25fc
mboti2000 <mboti20000@gmail.com>
2020-10-02 15:36:45 +0200

    createExcel commit

--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
+using Excel1 = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 
 namespace Excel
@@ -17,14 +17,53 @@ namespace Excel
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel1.Application xlApp;
+        Excel1.Workbook xlWB;
+        Excel1.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         public void LoadData() {
             Flats = context.Flats.ToList();
+        }
+
+        public void CreateExcel() {
+
+            try
+            {
+                
+               // xlApp = new Excel.Application();
+
+               
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                
+                xlSheet = xlWB.ActiveSheet;
+
+                
+                //CreateTable(); 
+
+               
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
         }
     }
 }
