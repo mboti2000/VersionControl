5496c769ba96b236fe84b498d00bad6e002caa1c
mboti2000 <mboti20000@gmail.com>
2020-10-09 08:25:04 +0200

    dataLoad commit

--- ValueAtRisk/ValueAtRisk/Form1.cs
+++ ValueAtRisk/ValueAtRisk/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,14 @@ namespace ValueAtRisk
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
