db754f744f3b1e25d15d45393f6d757cd5a04818
mboti2000 <mboti20000@gmail.com>
2020-10-16 09:18:55 +0200

    createDiagram commit

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MNB
@@ -26,49 +27,71 @@ namespace MNB
             dataGridView1.DataSource = Rates;
             getCurrencyRates();
             processXML();
+            createDiagram();
         }
 
-        public void getCurrencyRates() { 
+        private void getCurrencyRates() {
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
-            var request = new GetExchangeRatesRequestBody(){
-
-                currencyNames = "Euro",
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
                 startDate = "2020-01-01",
                 endDate = "2020-06-30"
-
             };
 
+         
             var response = mnbService.GetExchangeRates(request);
-             
-           result = response.GetExchangeRatesResult;
+
+            
+            result = response.GetExchangeRatesResult;
         }
 
-        public void processXML() {
+        private void processXML() {
 
             var xml = new XmlDocument();
             xml.LoadXml(result);
 
-            foreach (XmlElement el in xml.DocumentElement)
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
             {
-                RateData rd = new RateData();
-                Rates.Add(rd);
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                Rates.Add(rate);
 
-                rd.Date = DateTime.Parse(el.GetAttribute("date"));
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
 
-                var firstChild = (XmlElement)el.ChildNodes[0];
-                rd.Currency = firstChild.GetAttribute("curr");
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
 
-                var currUnit = Decimal.Parse(firstChild.GetAttribute("unit"));
-                var unitValue = Decimal.Parse(firstChild.InnerText);
-                if (currUnit!=0) {
-                    rd.Value = unitValue / currUnit;
-                }
-                
-
-
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
             }
+        }
+
+        private void createDiagram() {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+
+            series.BorderWidth =  2;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
         }
     }
 }
