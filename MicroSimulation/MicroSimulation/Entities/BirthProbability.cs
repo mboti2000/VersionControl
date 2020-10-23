bcc87c178457f78e183f5493bf0a2c891d51a026
mboti2000 <mboti20000@gmail.com>
2020-10-23 10:18:05 +0200

    Class creation commit

---
+++ MicroSimulation/MicroSimulation/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroSimulation.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int numberOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
---
+++ MicroSimulation/MicroSimulation/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroSimulation.Entities
+{
+    class Person
+    {
+        public int yearOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int numberOfChildren { get; set; }
+        public bool isALive { get; set; }
+
+        
+    }
+}
