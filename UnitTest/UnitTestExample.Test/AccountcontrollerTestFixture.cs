da67e5a2a169abb6cadabb483c0b91a8899eb9fa
mboti2000 <mboti20000@gmail.com>
2020-11-19 16:18:32 +0100

    email validation test commit

---
+++ UnitTest/UnitTestExample.Test/AccountcontrollerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult) {
+           var AC = new AccountController();
+            var validateResult = AC.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, validateResult);
+        }
+    }
+}
