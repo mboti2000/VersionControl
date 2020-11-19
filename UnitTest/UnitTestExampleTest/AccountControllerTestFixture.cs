c19a59770ec145f1ef9736ede77c8ab4c5008d22
mboti2000 <mboti20000@gmail.com>
2020-11-19 21:29:49 +0100

    fix commit

---
+++ UnitTest/UnitTestExampleTest/AccountControllerTestFixture.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExampleTest
+{
+    public class AccountControllerTestFixture
+    {
+        [Test,
+           TestCase("abcd1234", false),
+           TestCase("irf@uni-corvinus", false),
+           TestCase("irf.uni-corvinus.hu", false),
+           TestCase("irf@uni-corvinus.hu", true)
+           ]
+
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var AC = new AccountController();
+            var validateResult = AC.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, validateResult);
+        }
+    }
+}
---
+++ UnitTest/UnitTestExampleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExampleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExampleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69089257-f4bf-4c03-821c-a849244c7155")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
