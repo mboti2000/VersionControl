5a0efac81be6bd8986d007b2e7ec8df9c5100c4d
mboti2000 <mboti20000@gmail.com>
2020-11-26 16:51:12 +0100

    pick winner commit

--- EvolutionGame/EvolutionGame/Form1.cs
+++ EvolutionGame/EvolutionGame/Form1.cs
@@ -20,6 +20,8 @@ namespace EvolutionGame
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+
+        Brain winnerBrain = null;
         public Form1()
         {
             InitializeComponent();
@@ -48,6 +50,15 @@ namespace EvolutionGame
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
 
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
             gc.ResetCurrentLevel();
 
             foreach (var p in topPerformers)
